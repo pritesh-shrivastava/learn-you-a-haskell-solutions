26e24c58b67b182a3b9b279ba2430b288739ea19
pritesh-shrivastava <pritesh2312@gmail.com>
2020-08-15 20:30:32 +0530

    [PS] On Ch-5 Recursion - More Recursive Functions

---
+++ ch-05/recursion.hs
@@ -0,0 +1,14 @@
+maximum' [] = error "maximum of empty list"  
+maximum' [x] = x  
+maximum' (x:xs)   
+    | x > maxTail = x  
+    | otherwise = maxTail  
+    where maxTail = maximum' xs  
+
+-- A more elegant solution
+maximum' :: (Ord a) => [a] -> a  
+maximum' [] = error "maximum of empty list"  
+maximum' [x] = x  
+maximum' (x:xs) = max x (maximum' xs)
+
+--
